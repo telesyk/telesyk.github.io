51445594941221529daa26930d3fdcd0d5148419
Oles Tkachuk <telesyk@users.noreply.github.com>
2021-11-09 10:09:18 +0200

    update deploy; fix paths (#2)
    * add deploy.sh
    
    * add './docs'
    
    * update deploy; fix paths

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # build
-yarn build
+yarn run build
 
 # navigate into the build output directory
-cd dist
+cd docs
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:telesyk/telesyk.github.io.git gh-pages
+git push -f git@github.com:telesyk/telesyk.github.io.git main:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
