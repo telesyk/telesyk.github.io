dc1a034be0c3dd210508d343d58f1580276c4fa3
telesyk <telesyk89@gmail.com>
2023-02-21 19:12:41 +0200

    remove redundant helper

--- src/helpers/fetchData.js
+++
@@ -1,3 +0,0 @@
-const fetchData = () => {};
-
-export default fetchData;
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,9 +1,7 @@
 import setCapitalize from "./set-capitalize";
 import isPrefersDarkSheme from "./is-prefers-dark-sheme";
-import fetchData from "./fetchData";
 
 export {
   setCapitalize,
   isPrefersDarkSheme,
-  fetchData
 };
